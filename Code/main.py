e2722d0a6e1d722da6d061fb0b9a6cf8b60ce35f
Bucky Lee <2209203187@qq.com>
2022-02-14 10:30:45 +0800

    1

--- Code/main.py
+++ Code/main.py
@@ -1,73 +1,29 @@
-import hashlib
-import os
-import tarfile
-import zipfile
+import imageio as iio
 import requests
+from io import BytesIO
 import numpy as np
-import pandas as pd
-import torch
-from torch import nn
-
-# @save
-DATA_HUB = dict()
-DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
-
-
-def download(name, cache_dir=os.path.join('..', 'data')):  # @save
-    """下载一个DATA_HUB中的文件，返回本地文件名"""
-    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
-    url, sha1_hash = DATA_HUB[name]
-    os.makedirs(cache_dir, exist_ok=True)
-    fname = os.path.join(cache_dir, url.split('/')[-1])
-    if os.path.exists(fname):
-        sha1 = hashlib.sha1()
-        with open(fname, 'rb') as f:
-            while True:
-                data = f.read(1048576)
-                if not data:
-                    break
-                sha1.update(data)
-        if sha1.hexdigest() == sha1_hash:
-            return fname  # 命中缓存
-    print(f'正在从{url}下载{fname}...')
-    r = requests.get(url, stream=True, verify=True)
-    with open(fname, 'wb') as f:
-        f.write(r.content)
-    return fname
-
-
-def download_extract(name, folder=None):  # @save
-    """下载并解压zip/tar文件"""
-    fname = download(name)
-    base_dir = os.path.dirname(fname)
-    data_dir, ext = os.path.splitext(fname)
-    if ext == '.zip':
-        fp = zipfile.ZipFile(fname, 'r')
-    elif ext in ('.tar', '.gz'):
-        fp = tarfile.open(fname, 'r')
-    else:
-        assert False, '只有zip/tar文件可以被解压缩'
-    fp.extractall(base_dir)
-    return os.path.join(base_dir, folder) if folder else data_dir
-
-
-def download_all():  # @save
-    """下载DATA_HUB中的所有文件"""
-    for name in DATA_HUB:
-        download(name)
-
-
-DATA_HUB['kaggle_house_train'] = (  #@save
-    DATA_URL + 'kaggle_house_pred_train.csv',
-    '585e9cc93e70b39160e7921475f9bcd7d31219ce')
-
-DATA_HUB['kaggle_house_test'] = (  #@save
-    DATA_URL + 'kaggle_house_pred_test.csv',
-    'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    train_data = pd.read_csv('/Users/lee/GitHub/2022_Winter_ML/data/kaggle_house_pred_train.csv')
-    test_data = pd.read_csv('/Users/lee/GitHub/2022_Winter_ML/data/kaggle_house_pred_test.csv')
-    print(train_data.shape)
-    print(test_data.shape)
-    print(train_data.iloc[0:4, [0, 1, 2, 3, -3, -2, -1]])
+    im = iio.imread("Forest-House.jpg")
+    im = np.array(im)
+    im = np.sum(im, axis=2) / 3
+    im.shape
+    im = im.reshape(436, 658, 1)
+    print(im.shape)
+    im = im.reshape(-1, 1)
+    print(im.shape)
+    N = 4
+
+    center = np.random.randint(0, 255, N)
+
+    for i in range(5):
+        diff = abs(im - center)
+        arg = np.argmin(diff, axis=1)
+        new_center = []
+        for num in np.unique(arg):
+            t = im * (arg == num)
+            new_center.append(np.sum(t) / np.sum(arg == num))
+        center = np.array(new_center)
+
+    print(center)
